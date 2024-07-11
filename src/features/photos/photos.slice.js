424e3ed3d75d18f1db1a78b3b1a1b9fea3b44a5d
Hilla Landau Tahery <htahary@indeed.com>
2024-07-11 09:05:04 +0900

    redux assessment

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -10,23 +10,38 @@ const options = {
   name: 'photos',
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
-    // Task 1 Hint: You can use state.photos.unshift()
-    // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
-    // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
-    // Task 6 Hint: You can use state.photos.splice()
-    // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos.
+    addPhoto: (state, action) => {
+      state.photos.unshift({ id: state.photos.length + 1, caption: action.payload.caption, imageUrl: action.payload.imageUrl });
+    },
+    removePhoto: (state, action) => {
+      state.photos.splice(
+          state.photos.findIndex((photo) => photo.id === action.payload),
+          1
+      );
+    },
   },
 };
 
 const photosSlice = createSlice(options);
-
 export const { addPhoto, removePhoto } = photosSlice.actions;
 
 export default photosSlice.reducer;
 
 export const selectAllPhotos = (state) => state.photos.photos;
+
+
+// Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
 export const selectFilteredPhotos = (state) => {
-  // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const photos = selectAllPhotos(state);
+  const searchTerm = selectSearchTerm(state);
+  if(searchTerm){
+    return photos.filter((item) =>
+        item.caption.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+  else
+    return photos;
 };
+
+
